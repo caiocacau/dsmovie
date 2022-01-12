f709a514c18196018e86592c36171ba5b46ebd96
Caio Cacau <cacau.caio@gmail.com>
2022-01-12 17:39:09 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,5 +36,9 @@ public class Movie implements Serializable {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	@Setter(value = AccessLevel.NONE)
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 	
 }
