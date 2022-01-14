8f1852112efb4ced01d9bfcf38c612cdcf648b5d
Caio Cacau <cacau.caio@gmail.com>
2022-01-14 12:12:51 -0300

    First request

---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,3 @@
+// process.env.REACT_APP_BACKEND_URL -> TEm que ser assim, devido ao netfile (produção)
+// Cso não tenha definida, por padrão usa o http://localhost:8080
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
