cdf348ecd75101ab171330ad09983c89bed31f45
Caio Cacau <cacau.caio@gmail.com>
2022-01-14 15:59:34 -0300

    Ajuste netlify array

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
-// process.env.REACT_APP_BACKEND_URL -> TEm que ser assim, devido ao netfile (produção)
+// process.env.REACT_APP_BACKEND_URL -> TEm que ser assim, devido ao netlify (produção)
 // Cso não tenha definida, por padrão usa o http://localhost:8080
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
